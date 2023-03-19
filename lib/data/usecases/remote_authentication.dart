ff5bf7e43c44aa8d2ef1964e12d12513bef29cd0
patrick.bremm <patrick.bremm>
2023-03-19 15:21:09 -0300

    ensure RemoteAuthentication throws UnexpectedError on 400

--- lib/data/usecases/remote_authentication.dart
+++ lib/data/usecases/remote_authentication.dart
@@ -1,7 +1,8 @@
-import 'package:curso_flutter/domain/entities/entities.dart';
+
 import 'package:meta/meta.dart';
 
 import '../../domain/usecases/usecases.dart';
+import '../../domain/helpers/helpers.dart';
 
 import '../http/http.dart';
 
@@ -14,7 +15,11 @@ class RemoteAuthentication {
 
   Future<void> auth(AuthenticationParams params) async {
     final body = RemoteAuthenticationParams.fromDomain(params).toJson();
-    await httpClient.request(url: url, method: 'post', body: body);
+    try {
+      await httpClient.request(url: url, method: 'post', body: body);
+    } on HttpError {
+      throw DomainError.unexpected;
+    }    
   }
 }
 
--- test/data/usecases/remote_authentication_test.dart
+++ test/data/usecases/remote_authentication_test.dart
@@ -2,12 +2,12 @@ import 'package:faker/faker.dart';
 import 'package:mockito/mockito.dart';
 import 'package:test/test.dart';
 
+import 'package:curso_flutter/domain/helpers/domain_error.dart';
 import 'package:curso_flutter/domain/usecases/usecases.dart';
 
 import 'package:curso_flutter/data/usecases/usecases.dart';
 import 'package:curso_flutter/data/http/http_client.dart';
-
-
+import 'package:curso_flutter/data/http/http.dart';
 
 class HttpClientSpy extends Mock implements HttpClient {}
 
@@ -31,5 +31,15 @@ void main() {
         method: 'post',
         body: {'email': params.email, 'password': params.secret}
       ));
+  });
+
+  test('Should throw UnexpectedError if HttpClient returns 400', () async {
+    when(httpClient.request(url: anyNamed('url'), method: anyNamed('method'), body: anyNamed('body')))
+      .thenThrow(HttpError.badRequest);
+
+    final params = AuthenticationParams(email: faker.internet.email(), secret: faker.internet.password());
+    final future = sut.auth(params);
+
+    expect(future, throwsA(DomainError.unexpected));
   });
 }
