ab5b5718edfa126759613f5dcd1a80ce5129ff23
patrick.bremm <patrick.bremm>
2023-03-28 22:16:15 -0300

    move api details from domain entity to data model

---
+++ lib/data/models/remote_account_model.dart
@@ -0,0 +1,13 @@
+import 'package:curso_flutter/domain/entities/entities.dart';
+
+class RemoteAccountModel {
+    final String accessToken;
+
+    RemoteAccountModel(this.accessToken);
+
+    factory RemoteAccountModel.fromJson(Map json) =>
+      RemoteAccountModel(json['accessToken']);
+
+    AccountEntity toEntity() =>
+      AccountEntity(accessToken);
+}
--- lib/data/usecases/remote_authentication.dart
+++ lib/data/usecases/remote_authentication.dart
@@ -5,6 +5,7 @@ import '../../domain/usecases/usecases.dart';
 import '../../domain/helpers/helpers.dart';
 import '../../domain/entities/entities.dart';
 
+import '../models/models.dart';
 import '../http/http.dart';
 
 
@@ -18,7 +19,7 @@ class RemoteAuthentication {
     final body = RemoteAuthenticationParams.fromDomain(params).toJson();
     try {
       final httpResponse = await httpClient.request(url: url, method: 'post', body: body);
-      return AccountEntity.fromJson(httpResponse);
+      return RemoteAccountModel.fromJson(httpResponse).toEntity();
 
     } on HttpError catch(error) {
       throw error == HttpError.unauthorized 
