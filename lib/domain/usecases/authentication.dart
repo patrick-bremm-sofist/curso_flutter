0ecd1e1356aa41ca8f84c3b7df5a95c6ca447dc4
patrick.bremm <patrick.bremm>
2023-03-28 22:45:39 -0300

    make RemoteAuthentication implements Authentication

--- lib/domain/usecases/authentication.dart
+++ lib/domain/usecases/authentication.dart
@@ -3,7 +3,7 @@ import 'package:meta/meta.dart';
 import '../entities/account_entity.dart';
 
 abstract class Authentication {
-    Future<AccountEntity> auth({AuthenticationParams params});
+    Future<AccountEntity> auth(AuthenticationParams params);
 }
 
 class AuthenticationParams {
