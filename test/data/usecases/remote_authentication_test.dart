b815c921d2a9d4574df180adb33be2784c866ca7
patrick.bremm <patrick.bremm>
2023-03-28 22:25:29 -0300

    ensure RemoteAuthentication throws UnexpectedError on 200 with invalid data

--- test/data/usecases/remote_authentication_test.dart
+++ test/data/usecases/remote_authentication_test.dart
@@ -83,4 +83,13 @@ void main() {
 
     expect(account.token, accessToken);
   });
+
+  test('Should throw UnexpectedError an Account if HttpClient returns 200 with invalid data', () async {
+    when(httpClient.request(url: anyNamed('url'), method: anyNamed('method'), body: anyNamed('body')))
+      .thenAnswer((_) async => {'invalid_key': 'invalid_value'});
+
+    final future = sut.auth(params);
+
+    expect(future, throwsA(DomainError.unexpected));
+  });
 }
