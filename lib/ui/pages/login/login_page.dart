92a88233e26bb7812d9bb6604a752480d0299368
patrick.bremm <patrick.bremm>
2023-05-21 17:33:38 -0300

    ensure LoginPage calls authentication on form submit

--- lib/ui/pages/login/login_page.dart
+++ lib/ui/pages/login/login_page.dart
@@ -61,7 +61,7 @@ class LoginPage extends StatelessWidget {
                   stream: presenter.isFormValidStream,
                   builder: (context, snapshot) {
                     return RaisedButton(
-                      onPressed: snapshot.data == true ? () {} : null,
+                      onPressed: snapshot.data == true ? presenter.auth : null,
                       child: Text('Entrar'.toUpperCase()),
                     );
                   }),
