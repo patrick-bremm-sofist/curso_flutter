c2bdcc20aa9faa7cd97aa8cae36e9d63abe93d52
patrickbremm <patrick.bremm@sofist.co>
2023-11-07 15:57:11 -0300

    move StreamLoginPresenter to its own file

--- test/presentation/presenters/stream_login_presenter_test.dart
+++ test/presentation/presenters/stream_login_presenter_test.dart
@@ -1,30 +1,9 @@
-import 'dart:async';
-
-import 'package:curso_flutter/presentation/protocols/validation.dart';
-import 'package:meta/meta.dart';
 import 'package:mockito/mockito.dart';
 import 'package:test/test.dart';
 import 'package:faker/faker.dart';
 
-class LoginState {
-  String emailError;
-}
-
-class StreamLoginPresenter {
-  final Validation validation;
-  final _controller = StreamController<LoginState>.broadcast();
-
-  var _state = LoginState();
-
-  Stream<String> get emailErrorStream => _controller.stream.map((state) => state.emailError);
-
-  StreamLoginPresenter({@required this.validation});
-
-  void validateEmail(String email) {
-    _state.emailError = validation.validate(field: 'email', value: email);
-    _controller.add(_state);
-  }
-}
+import 'package:curso_flutter/presentation/protocols/protocols.dart';
+import 'package:curso_flutter/presentation/presenters/presenters.dart';
 
 class ValidationSpy extends Mock implements Validation {}
 
