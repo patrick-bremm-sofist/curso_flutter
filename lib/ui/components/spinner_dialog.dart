9523e15ffd936a68bac94d9fae8317a11fb7c461
patrickbremm <patrick.bremm@sofist.co>
2023-11-07 15:20:26 -0300

    ensure StreamLoginPresenter calls Validation on email changed

--- lib/ui/components/spinner_dialog.dart
+++ lib/ui/components/spinner_dialog.dart
@@ -1,22 +1,22 @@
 import 'package:flutter/material.dart';
 
-Future<void> showLoading(BuildContext context) async {
-  await Future.delayed(Duration.zero);
-  await showDialog(
-      context: context,
-      barrierDismissible: false,
-      builder: (context) => SimpleDialog(
-        children: <Widget>[
-          Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              CircularProgressIndicator(),
-              SizedBox(height: 10),
-              Text('Aguarde...', textAlign: TextAlign.center)
-            ],
-          )
-        ],
-      ));
+void showLoading(BuildContext context) {
+  showDialog(
+    context: context,
+    barrierDismissible: false,
+    child: SimpleDialog(
+      children: <Widget>[
+        Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            CircularProgressIndicator(),
+            SizedBox(height: 10),
+            Text('Aguarde...', textAlign: TextAlign.center),
+          ],
+        ),
+      ],
+    ),
+  );
 }
 
 void hideLoading(BuildContext context) {
---
+++ test/presentations/presenters/stream_login_presenter_test.dart
@@ -0,0 +1,32 @@
+import 'package:meta/meta.dart';
+import 'package:mockito/mockito.dart';
+import 'package:test/test.dart';
+import 'package:faker/faker.dart';
+
+abstract class Validation {
+  String validate({@required field, @required String value});
+}
+
+class StreamLoginPresenter {
+  final Validation validation;
+
+  StreamLoginPresenter({@required this.validation});
+
+  void validateEmail(String email) {
+    validation.validate(field: 'email', value: email);
+  }
+}
+
+class ValidationSpy extends Mock implements Validation {}
+
+void main() {
+  test('Should call Validation with correct email', () {
+    final validation = ValidationSpy();
+    final sut = StreamLoginPresenter(validation: validation);
+    final email = faker.internet.email();
+
+    sut.validateEmail(email);
+
+    verify(validation.validate(field: 'email', value: email)).called(1);
+  });
+}
