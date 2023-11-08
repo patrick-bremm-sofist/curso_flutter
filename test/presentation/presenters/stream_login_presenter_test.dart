10067ff8bf93fd9b3508f1825f95e62a0564e756
patrickbremm <patrick.bremm@sofist.co>
2023-11-08 11:26:30 -0300

    ensure StreamLoginPresenter emits correct events on Authentication success

--- test/presentation/presenters/stream_login_presenter_test.dart
+++ test/presentation/presenters/stream_login_presenter_test.dart
@@ -2,7 +2,8 @@ import 'package:mockito/mockito.dart';
 import 'package:test/test.dart';
 import 'package:faker/faker.dart';
 
-import 'package:curso_flutter/domain/usecases/authentication.dart';
+import 'package:curso_flutter/domain/entities/entities.dart';
+import 'package:curso_flutter/domain/usecases/usecases.dart';
 
 import 'package:curso_flutter/presentation/protocols/protocols.dart';
 import 'package:curso_flutter/presentation/presenters/presenters.dart';
@@ -24,6 +25,12 @@ void main() {
     mockValidationCall(field).thenReturn(value);
   }
 
+  PostExpectation mockAuthenticationCall() => when(authentication.auth(any));
+
+  void mockAuthentication() {
+    mockAuthenticationCall().thenAnswer((_) async => AccountEntity(faker.guid.guid()));
+  }
+
   setUp(() {
     validation = ValidationSpy();
     authentication = AuthenticationSpy();
@@ -31,6 +38,7 @@ void main() {
     email = faker.internet.email();
     password = faker.internet.password();
     mockValidation();
+    mockAuthentication();
   });
 
   test('Should call Validation with correct email', () {
@@ -109,5 +117,14 @@ void main() {
     await sut.auth();
 
     verify(authentication.auth(AuthenticationParams(email: email, secret: password))).called(1);
+  });
+  
+  test('Should emit correct events on Authentication success', () async {
+    sut.validateEmail(email);
+    sut.validatePassword(password);
+
+    expectLater(sut.isLoadingStream, emitsInOrder([true, false]));
+
+    await sut.auth();
   });
 }
