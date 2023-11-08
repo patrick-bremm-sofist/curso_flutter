fc9187b39030752821b7f5bc1d4a811e9c188263
patrickbremm <patrick.bremm@sofist.co>
2023-11-08 14:36:32 -0300

    ensure EmailValidation returns error if email is invalid

--- test/validation/validators/email_validation_test.dart
+++ test/validation/validators/email_validation_test.dart
@@ -1,6 +1,6 @@
-import 'package:curso_flutter/validation/protocols/field_validation.dart';
 import 'package:test/test.dart';
 
+import 'package:curso_flutter/validation/protocols/field_validation.dart';
 
 class EmailValidation implements FieldValidation {
   final String field;
@@ -8,7 +8,9 @@ class EmailValidation implements FieldValidation {
   EmailValidation(this.field);
 
   String validate(String value) {
-    return null;
+    final regex = RegExp(r"^[a-zA-Z0-9_.-]+@[a-zA-Z0-9-]+.[a-zA-Z0-9-.]+$");
+    final isValid = value?.isNotEmpty != true || regex.hasMatch(value);
+    return isValid ? null : 'Campo inválido';
   }
 }
 
@@ -29,5 +31,9 @@ void main() {
 
   test('Should return null if email is valid', () {
     expect(sut.validate('patrick.bremm@sofist.co'), null);
+  });
+
+  test('Should return error if email is invalid', () {
+    expect(sut.validate('patrick.bremm'), 'Campo inválido');
   });
 }
