d4708660c6a1de06b6a9cee22991991c11d2d367
patrickbremm <patrick.bremm@sofist.co>
2023-11-09 15:58:46 -0300

    add GetxLoginPresenter

--- lib/main/factories/pages/login/login_presenter_factory.dart
+++ lib/main/factories/pages/login/login_presenter_factory.dart
@@ -3,8 +3,15 @@ import '../../../../ui/pages/pages.dart';
 
 import '../../factories.dart';
 
-LoginPresenter makeLoginPresenter() {
+LoginPresenter makeStreamLoginPresenter() {
   return StreamLoginPresenter(
+    authentication: makeRemoteAuthentication(),
+    validation: makeLoginValidation()
+  );
+}
+
+LoginPresenter makeGetxLoginPresenter() {
+  return GetxLoginPresenter(
     authentication: makeRemoteAuthentication(),
     validation: makeLoginValidation()
   );
