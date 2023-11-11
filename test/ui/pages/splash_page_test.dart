6e2ab22afaf7be9f50b00b3deeb234ed8647e387
patrickbremm <patrick.bremm@sofist.co>
2023-11-11 13:05:50 -0300

    ensure SplashPage calls loadCurrentAccount on page load

--- test/ui/pages/splash_page_test.dart
+++ test/ui/pages/splash_page_test.dart
@@ -1,11 +1,18 @@
-import 'package:flutter/cupertino.dart';
-import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:meta/meta.dart';
+import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:mockito/mockito.dart';
 
 class SplashPage extends StatelessWidget {
+  final SplashPresenter presenter;
+
+  SplashPage({@required this.presenter});
+  
   @override
   Widget build(BuildContext context) {
+    presenter.loadCurrentAccount();
+
     return Scaffold(
       appBar: AppBar(title: Text('4Dev'),),
       body: Center(
@@ -15,13 +22,22 @@ class SplashPage extends StatelessWidget {
   }
 }
 
+abstract class SplashPresenter {
+  Future<void> loadCurrentAccount();
+}
+
+class SplashPresenterSpy extends Mock implements SplashPresenter {}
+
 void main() {
+  SplashPresenterSpy presenter;
+
   Future<void> loadPage(WidgetTester tester) async {
+    presenter = SplashPresenterSpy();
     await tester.pumpWidget(
       GetMaterialApp(
         initialRoute:  '/',
         getPages: [
-          GetPage(name: '/', page: () => SplashPage())
+          GetPage(name: '/', page: () => SplashPage(presenter: presenter))
         ],
       )
     );
@@ -31,5 +47,11 @@ void main() {
     await loadPage(tester);
 
     expect(find.byType(CircularProgressIndicator), findsOneWidget);
+  });
+
+  testWidgets('Should call loadingCurrentAccount on page load', (WidgetTester tester) async {
+    await loadPage(tester);
+
+    verify(presenter.loadCurrentAccount()).called(1);
   });
 }
