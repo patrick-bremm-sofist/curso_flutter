0988bc29f12a592d28d58975837368c836c9745f
patrickbremm <patrick.bremm@sofist.co>
2023-11-13 11:04:47 -0300

    ensure GetxSplashPresenter calls LoadCurrentAccount

--- test/ui/pages/splash_page_test.dart
+++ test/ui/pages/splash_page_test.dart
@@ -43,7 +43,7 @@ void main() {
   testWidgets('Should call loadingCurrentAccount on page load', (WidgetTester tester) async {
     await loadPage(tester);
 
-    verify(presenter.loadCurrentAccount()).called(1);
+    verify(presenter.checkAccount()).called(1);
   });
 
   testWidgets('Should change page', (WidgetTester tester) async {
