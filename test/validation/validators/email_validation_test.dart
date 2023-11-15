c1a1c6648411208348393645e5e552de7c48f762
patrickbremm <patrick.bremm@sofist.co>
2023-11-15 16:28:41 -0300

    add invalid cases test for EmailValidation

--- test/validation/validators/email_validation_test.dart
+++ test/validation/validators/email_validation_test.dart
@@ -10,8 +10,8 @@ void main() {
     sut = EmailValidation('any_field');
   });
 
-  test('Should return null if email is empty', () {
-    expect(sut.validate({'any_field': ''}), null);
+  test('Should return null on invalid case', () {
+    expect(sut.validate({}), null);
   });
 
   test('Should return null if email is null', () {
