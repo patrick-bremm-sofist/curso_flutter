083c1b68fa10ff17f302fa4e3b876e52ec6ce421
patrickbremm <patrick.bremm@sofist.co>
2023-11-16 11:55:39 -0300

    move SUT creation to the setup method

--- test/data/usecases/remote_load_surveys_test.dart
+++ test/data/usecases/remote_load_surveys_test.dart
@@ -19,10 +19,18 @@ class RemoteLoadSurveys {
 class HttpClientSpy extends Mock implements HttpClient {}
 
 void main() {
+  RemoteLoadSurveys sut;
+  HttpClientSpy httpClient;
+  String url;
+
+  setUp(() {
+    url = faker.internet.httpUrl();
+    httpClient = HttpClientSpy();
+    sut = RemoteLoadSurveys(url: url, httpClient: httpClient);
+  });
+  
   test('Should call HttpClient with correct values', () async {
-    final url = faker.internet.httpUrl();
-    final httpClient = HttpClientSpy();
-    final sut = RemoteLoadSurveys(url: url, httpClient: httpClient);
+    
 
     await sut.load();
 
