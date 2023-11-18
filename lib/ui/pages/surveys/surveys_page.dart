48665c7f4739859a0b99e9b10e5746c1b2ebec10
patrickbremm <patrick.bremm@sofist.co>
2023-11-18 12:19:19 -0300

    ensure SurveysPage calls loadData on page load

--- lib/ui/pages/surveys/surveys_page.dart
+++ lib/ui/pages/surveys/surveys_page.dart
@@ -3,10 +3,16 @@ import 'package:carousel_slider/carousel_slider.dart';
 
 import 'components/components.dart';
 import '../../helpers/helpers.dart';
+import 'surveys_presenter.dart';
 
 class SurveysPage extends StatelessWidget {
+  final SurveysPresenter presenter;
+
+  SurveysPage(this.presenter);
+
   @override
   Widget build(BuildContext context) {
+    presenter.loadData();
     return Scaffold(
       appBar: AppBar(title: Text(R.strings.surveys)),
       body: Padding(
