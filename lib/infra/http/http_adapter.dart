f408589b816093e15ee1e68efd9df325cd23e5d0
patrickbremm <patrick.bremm@sofist.co>
2023-11-20 14:50:10 -0300

    add headers to HttpClient

--- lib/infra/http/http_adapter.dart
+++ lib/infra/http/http_adapter.dart
@@ -10,19 +10,19 @@ class HttpAdapter implements HttpClient {
 
   HttpAdapter(this.client);
 
-  Future<Map> request(
-      {@required String url, @required String method, Map body}) async {
-    final headers = {
+  Future<dynamic> request(
+      {@required String url, @required String method, Map body, Map headers}) async {
+    final defaultheaders = headers?.cast<String, String>() ?? {}..addAll({
       'content-type': 'application/json',
       'accept': 'application/json'
-    };
+    });
     final jsonBody = body != null ? jsonEncode(body) : null;
     var response = Response('', 500);
     try {
       if (method == 'post') {
-        response = await client.post(url, headers: headers, body: jsonBody);
+        response = await client.post(url, headers: defaultheaders, body: jsonBody);
       } else if (method == 'get') {
-        response = await client.get(url, headers: headers);
+        response = await client.get(url, headers: defaultheaders);
       }
     } catch (error) {
       throw HttpError.serverError;
@@ -30,7 +30,7 @@ class HttpAdapter implements HttpClient {
     return _handleResponse(response);
   }
 
-  Map _handleResponse(Response response) {
+  dynamic _handleResponse(Response response) {
     if (response.statusCode == 200) {
       return response.body.isEmpty ? null : jsonDecode(response.body);
     } else if (response.statusCode == 204) {
--- test/infra/http/http_adapter_test.dart
+++ test/infra/http/http_adapter_test.dart
@@ -45,15 +45,28 @@ void main() {
     });
 
     test('Should call post with correct values', () async {
-      await sut
-          .request(url: url, method: 'post', body: {'any_key': 'any_value'});
+      await sut.request(url: url, method: 'post', body: {'any_key': 'any_value'});
 
-      verify(client.post(url,
+      verify(client.post(
+          url,
           headers: {
             'content-type': 'application/json',
             'accept': 'application/json'
           },
-          body: '{"any_key":"any_value"}'));
+          body: '{"any_key":"any_value"}'
+      ));
+      
+      await sut.request(url: url, method: 'post', body: {'any_key': 'any_value'}, headers: {'any=header': 'any_value'});
+
+      verify(client.post(
+          url,
+          headers: {
+            'content-type': 'application/json',
+            'accept': 'application/json',
+            'any=header': 'any_value'
+          },
+          body: '{"any_key":"any_value"}'
+      ));
     });
 
     test('Should call post without body', () async {
@@ -178,6 +191,16 @@ void main() {
           headers: {
             'content-type': 'application/json',
             'accept': 'application/json'
+      }));
+
+      await sut
+          .request(url: url, method: 'get', headers: {'any_header': 'any_value'});
+
+      verify(client.get(url,
+          headers: {
+            'content-type': 'application/json',
+            'accept': 'application/json',
+            'any_header': 'any_value'
       }));
     });
 
