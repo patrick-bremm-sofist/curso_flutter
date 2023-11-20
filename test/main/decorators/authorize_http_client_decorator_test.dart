bf1c2bf74ec636da3013070a8b0dd7d0349f05ab
patrickbremm <patrick.bremm@sofist.co>
2023-11-20 15:34:06 -0300

    ensure AuthorizeHttpClientDecorator calls FetchSecureCacheStorage with correct key

---
+++ test/main/decorators/authorize_http_client_decorator_test.dart
@@ -0,0 +1,29 @@
+import 'package:meta/meta.dart';
+import 'package:mockito/mockito.dart';
+import 'package:test/test.dart';
+
+import 'package:curso_flutter/data/cache/cache.dart';
+
+class AuthorizeHttpClientDecorator{
+  final FetchSecureCacheStorage fetchSecureCacheStorage;
+
+  AuthorizeHttpClientDecorator({@required this.fetchSecureCacheStorage});
+
+  Future<void> request() async {
+    await fetchSecureCacheStorage.fetchSecure('token');
+  }
+}
+
+class FetchSecureCacheStorageSpy extends Mock implements FetchSecureCacheStorage {}
+
+void main() {
+  test('Should call FetchSecureCacheStorage with correct key', () async {
+    final fetchSecureCacheStorage = FetchSecureCacheStorageSpy();
+    final sut = AuthorizeHttpClientDecorator(
+      fetchSecureCacheStorage: fetchSecureCacheStorage
+    );
+
+    await sut.request();
+    verify(fetchSecureCacheStorage.fetchSecure('token')).called(1);
+  });
+}
