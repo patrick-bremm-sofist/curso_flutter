502f2bd8d28e7f4fbd17f40c204cb528a9068689
patrickbremm <patrick.bremm@sofist.co>
2023-11-22 17:18:07 -0300

    ensure LocalLoadSurveys throws UnexpectedError if cache throws

--- test/data/usecases/load_surveys/local_load_surveys._test.dart
+++ test/data/usecases/load_surveys/local_load_surveys._test.dart
@@ -192,6 +192,10 @@ void main() {
     CacheStorageSpy cacheStorage;
     List<SurveyEntity> surveys;
 
+    PostExpectation mockSaveCall() => when(cacheStorage.save(key: anyNamed('key'), value: anyNamed('value')));
+
+    void mockSaveError() => mockSaveCall().thenThrow(Exception());
+
     List<SurveyEntity> mockSurveys() => [
       SurveyEntity(
         id: faker.guid.guid(), 
@@ -232,6 +236,14 @@ void main() {
       await sut.save(surveys);
 
       verify(cacheStorage.save(key: 'surveys', value: list)).called(1);
+    });
+    
+    test('Should throw CacheStorage if save throws', () async {
+      mockSaveError();
+
+      final future = sut.save(surveys);
+
+      expect(future, throwsA(DomainError.unexpected));
     });
   });
 }
