9c9999501a1772f18c68c56167c5b9b975ddd1a7
patrickbremm <patrick.bremm@sofist.co>
2023-11-24 14:37:17 -0300

    add makeRemoteoadSurveysWithLocalFallback factory

--- lib/main/factories/usecases/load_surveys_factory.dart
+++ lib/main/factories/usecases/load_surveys_factory.dart
@@ -1,10 +1,25 @@
+
 import '../../../data/usecases/usecases.dart';
 import '../../../domain/usecases/usecases.dart';
+import '../../composites/composites.dart';
 import '../factories.dart';
 
 LoadSurveys makeRemoteLoadSurveys() {
   return RemoteLoadSurveys(
     httpClient: makeAuthorizeHttpClientDecorator(), 
     url: makeApiUrl('surveys')
+  );
+}
+
+LoadSurveys makeLocalLoadSurveys() {
+  return LocalLoadSurveys(
+    cacheStorage: makeLocalStorageAdapter()
+  );
+}
+
+LoadSurveys makeRemoteoadSurveysWithLocalFallback() {
+  return RemoteLoadSurveysWithLocalFallback(
+    remote: makeRemoteLoadSurveys(),
+    local: makeLocalLoadSurveys()
   );
 }
