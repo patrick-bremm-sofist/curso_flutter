b2dac0e8b2b89a5dc9e5fc8ac0050f33ba408cbe
patrickbremm <patrick.bremm@sofist.co>
2023-11-30 12:14:32 -0300

    ensure SurveyResultPage presents loading correctly

--- lib/ui/pages/survey_result/survey_result_page.dart
+++ lib/ui/pages/survey_result/survey_result_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 
+import '../../components/components.dart';
 import '../../helpers/helpers.dart';
 import './survey_result.dart';
 
@@ -14,8 +15,15 @@ class SurveyResultPage extends StatelessWidget {
       appBar: AppBar(title: Text(R.strings.surveys)),
       body: Builder(
           builder: (context) {
+            presenter.isLoadingStream.listen((isLoading) {
+              if (isLoading == true) {
+                showLoading(context);
+              } else {
+                hideLoading(context);
+              }
+            });
             presenter.loadData();
-            
+
             return ListView.builder(
             itemBuilder: (context, index) {
               if (index == 0) {
